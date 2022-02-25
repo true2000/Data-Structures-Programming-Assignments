cb8f90a712be082459907826fc701f283ea0270c
true2000 <57641483+true2000@users.noreply.github.com>
2022-02-24 19:45:36 -0600

    Update Programming Assignment #2 (Sorting).cpp

--- Programming Assignment #2 (Sorting).cpp
+++ Programming Assignment #2 (Sorting).cpp
@@ -6,7 +6,8 @@ Each boy wants to dance with a girl who is strictly shorter than him. A girl can
 Given the heights of all the boys and girls tell whether it is possible for all boys to dance with a girl.
 
 Input: The first line contains T denoting the number of test cases. Each test case contains three lines. 
-The first line contains M and N. The second line contains M integers each denoting the height of boy. 
+The first line contains M and N. 
+The second line contains M integers each denoting the height of boy. 
 The third contains N integers each denoting the height of girl.
 
 Output: Print YES if it is possible for each boy to dance with a girl else print NO.
