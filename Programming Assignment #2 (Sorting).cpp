d6e5feebc96e1e551c2dd2f7fc02321fe441bb8e
true2000 <57641483+true2000@users.noreply.github.com>
2022-02-24 19:44:46 -0600

    Update Programming Assignment #2 (Sorting).cpp

--- Programming Assignment #2 (Sorting).cpp
+++ Programming Assignment #2 (Sorting).cpp
@@ -1,3 +1,28 @@
+*********** Instructions ***********
+Sorting
+
+Riverfield Country Day School will be hosting its Prom Night tonight.There would be M boys and N girls at the prom tonight. 
+Each boy wants to dance with a girl who is strictly shorter than him. A girl can dance with only one boy and vice-versa. 
+Given the heights of all the boys and girls tell whether it is possible for all boys to dance with a girl.
+
+Input: The first line contains T denoting the number of test cases. Each test case contains three lines. 
+The first line contains M and N. The second line contains M integers each denoting the height of boy. 
+The third contains N integers each denoting the height of girl.
+
+Output: Print YES if it is possible for each boy to dance with a girl else print NO.
+
+Sample input:
+
+1
+4 5
+2 5 6 8
+3 8 5 1 7
+Output from sample input:
+
+YES
+
+************************************
+
 #include <iostream>
 using namespace std;
 
